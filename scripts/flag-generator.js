d3f2a105bda5eb9c31344032c469671c698b350a
Feufochmar <feufochmar.gd@gmail.com>
2015-08-03 23:34:58 +0200

    Import elements from the flag generator

---
+++ scripts/flag-generator.js
@@ -0,0 +1,13 @@
+function getFlag() {
+  document.getElementById('flag').innerHTML = '';
+  document.getElementById('raw').innerHTML = '';
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        document.getElementById('flag').innerHTML = request.responseText;
+        document.getElementById('raw').innerHTML = request.responseText;
+      }
+    };
+  request.open('GET', '/FlagGenerator/RawFlag', false);
+  request.send(null);
+}
